- fix TR when using cartesian_readout - fix counters for 2D/3D cartesian_readout - correct se2d-readout gradient ramp-up and slice selective refocusing shape - class info: add dz and is3D values

ReadoutBlocks/readoutWaveformGenerator.py
@@ -350,7 +350,7 @@ def spiral_varden(fov, res, gts, gslew, gamp, densamp, dentrans, nl, rewinder=Fa
 
     return g, k, t, s, dens
 
-def cartesian(fov, n, dt, gamp, gslew, dirx=-1, diry=1):
+def cartesian(fov, n, dt, gamp, gslew, infos, dirx=-1, diry=1, dirz=1):
     r"""Basic cartesian single-line readout designer.
 
     Args:
@@ -360,9 +360,10 @@ def cartesian(fov, n, dt, gamp, gslew, dirx=-1, diry=1):
         dt (float): sample time in s.
         gamp (float): max gradient amplitude in mT/m.
         gslew (float): max slew rate in mT/m/ms.
-        offset (int): used for multi-shot EPI goes from 0 to #shots-1
-        dirx (int): x direction of EPI -1 left to right, 1 right to left
-        diry (int): y direction of EPI -1 bottom-top, 1 top-bottom
+        infos (Info): structure with sequence information
+        dirx (int): x direction, -1 left to right, 1 right to left
+        diry (int): y direction, -1 posterior-anterior, 1 anterior-posterior
+        dirz (int): z direction, -1 feet-head, 1 head-feet
 
     Returns:
         tuple: (g, k, t, s) tuple containing
@@ -376,7 +377,8 @@ def cartesian(fov, n, dt, gamp, gslew, dirx=-1, diry=1):
     References:
         Adapted fron EPI version. 
     """
-    part_num = 64 # for 3D
+    is3D = infos.is3D
+    
     s = gslew * dt * 1000
     scaley = 20 # original value 20
 
@@ -393,11 +395,8 @@ def cartesian(fov, n, dt, gamp, gslew, dirx=-1, diry=1):
     gxro = g * np.ones((1, n))  # plateau of readout trapezoid
     areapd = np.sum(gxro) * dt
 
-    ramp = np.expand_dims(np.linspace(s, g, int(g / s)), axis=0)
-    gxro = np.concatenate(
-        (np.expand_dims(np.array([0]), axis=1), ramp, gxro, np.fliplr(ramp)),
-        axis=1,
-    )
+    ramp = np.expand_dims(np.linspace(0, g, int(np.ceil(g / s))+1), axis=0)
+    gxro = np.concatenate((ramp, gxro, np.fliplr(ramp)),axis=1)
 
     # x prewinder. make sure res_kpre is even. Handle even N by changing prew.
     if n % 2 == 0:
@@ -410,7 +409,6 @@ def cartesian(fov, n, dt, gamp, gslew, dirx=-1, diry=1):
         (np.zeros((1, (gxprew.size + ramp.size) % 2)), gxprew), axis=1
     )
 
-
     # phase-encode trapezoids before/after gx
     # handle even N by changing prewinder
     if n % 2 == 0:
@@ -421,26 +419,24 @@ def cartesian(fov, n, dt, gamp, gslew, dirx=-1, diry=1):
     gyprew = diry * trap_grad(areayprew, gamp, gslew  * 1000, dt)[0]
     gyprew = np.concatenate((np.zeros((1, gyprew.size % 2)), gyprew), axis=1)
 
-    gxro = -dirx * gxro
-
     # add rephasers at end of gx and gy readout
     areagy = -areayprew  # units = G/cm*s
     gyrep = trap_grad(areagy, gamp, gslew * 1000, dt)[0]
     # gy = np.concatenate((gy, gyrep), axis=1)
 
     ##for 3D
-    # partition-encode trapezoids
-    gslab = g * np.ones((1, part_num))  # test for 5 slabs
-    areapdz = np.sum(gslab) * dt
-    if part_num % 2 == 0:
-        areazprew = areapdz / 2 - g * dt
-    else:
-        areazprew = (areapdz - g * dt) / 2 - g * dt
-    gzprew = diry * trap_grad(areazprew, gamp, gslew  * 1000, dt)[0]
-    gzprew = np.concatenate((np.zeros((1, gzprew.size % 2)), gzprew), axis=1)
+    if is3D:
+        # partition-encode trapezoids
+        gslab = g * np.ones((1, infos.slices))  # test for 5 slabs
+        areapdz = np.sum(gslab) * dt
+        if infos.slices % 2 == 0:
+            areazprew = areapdz / 2 - g * dt
+        else:
+            areazprew = (areapdz - g * dt) / 2 - g * dt
+        gzprew = dirz * trap_grad(areazprew, gamp, gslew  * 1000, dt)[0]
+        gzprew = np.concatenate((np.zeros((1, gzprew.size % 2)), gzprew), axis=1)
     ##for 3D
     
-
     areagx = area
     gxrep = trap_grad(-areagx, gamp, gslew * 1000, dt)[0]
 
@@ -450,23 +446,24 @@ def cartesian(fov, n, dt, gamp, gslew, dirx=-1, diry=1):
         sign = -1
     gyprew_max_ampl = max(abs(gyprew[0]))
     step = gyprew_max_ampl / n
-    gyprew_equation = str(sign) + "*(" + str(gyprew_max_ampl) + "-" + str(2*step) +"*counter3)"
+    gyprew_equation = str(sign) + "*(" + str(gyprew_max_ampl) + "-" + str(2*step) +"*counterPE)"
 
     sign = 1
     if areagy<0:
         sign = -1
     gyrep_max_ampl = max(abs(gyrep[0]))
     step = gyprew_max_ampl / n
-    gyrep_equation = str(sign) + "*(" + str(gyrep_max_ampl) + "-" + str(2*step) +"*counter3)"
+    gyrep_equation = str(sign) + "*(" + str(gyrep_max_ampl) + "-" + str(2*step) +"*counterPE)"
 
     ##for 3D
-    # Prepare dynamic partition encoding
-    signz = 1
-    if areazprew<0:
-        signz = -1
-    gzprew_max_ampl = max(abs(gzprew[0]))
-    stepz = gzprew_max_ampl / part_num # for part_num slabs
-    gzprew_equation = str(signz) + "*(" + str(gzprew_max_ampl) + "-" + str(2*stepz) +"*counter2)"
+    if is3D:
+        # Prepare dynamic partition encoding
+        signz = 1
+        if areazprew<0:
+            signz = -1
+        gzprew_max_ampl = max(abs(gzprew[0]))
+        stepz = gzprew_max_ampl / infos.slices # for part_num slabs
+        gzprew_equation = str(signz) + "*(" + str(gzprew_max_ampl) + "-" + str(2*stepz) +"*counter3D)"
     ##for 3D
     
     # prepare blocks for mtrk
@@ -489,11 +486,6 @@ def cartesian(fov, n, dt, gamp, gslew, dirx=-1, diry=1):
                "phase", 
                gyprew_equation, 
                gyprew_startTime],
-              [gzprew[0]/max(gzprew[0], key=abs),  ##for 3D
-               gzprew.size,
-               "slice", 
-               gzprew_equation, 
-               gzprew_startTime],
               [gxro[0]/max(gxro[0], key=abs), 
                gxro.size,
                "read", 
@@ -513,13 +505,20 @@ def cartesian(fov, n, dt, gamp, gslew, dirx=-1, diry=1):
                gyrep.size,
                "phase", 
                gyrep_equation, 
-               gyrep_startTime]]  
+               gyrep_startTime]]
+    if is3D:
+        block1.append([gzprew[0]/max(gzprew[0], key=abs),  ##for 3D
+               gzprew.size,
+               "slice", 
+               gzprew_equation, 
+               gzprew_startTime])
     
     blocks = [block1]
 
-    time_before_center = (gxro_startTime + (gxro.size/2) *dt) * 1e2
+    time_before_center = (adc1_startTime * 1e2 + (adc.size/2) *dt * 1e3)
+    time_after_center = gyrep_startTime * 1e2 + (gyrep.size)*dt * 1e3 - time_before_center
 
-    return blocks, time_before_center
+    return blocks, time_before_center, time_after_center
 
 def radial(fov, n_spokes, theta, dt, gamp, gslew):
     r"""Basic radial single-line readout designer.

se2d_generator.py
@@ -48,6 +48,8 @@ def se2d_generator():
         description="Spin Echo 2D Sequence",
         slices=1,
         fov=260,
+        dz=5,
+        is3D=False,
         pelines=128,
         seqstring="YARRA",
         reconstruction="%SiemensIceProgs%\\IceProgram2D")
@@ -68,7 +70,7 @@ def se2d_generator():
     gamma = 42.576e6  # Hz/T
     time_bw_product = 2.7
     BW = time_bw_product/(2560e-6)  # RF bandwidth in Hz
-    dz = 0.005  # slice thickness in meters
+    dz = sequence_data.infos.dz * 1e-3 # slice thickness in meters
     G_slice = BW / (gamma * dz)  # in T/m
     grad_slisel_amplitude = round(G_slice * 1e3, 2)  # convert to mT/m
 
@@ -79,12 +81,12 @@ def se2d_generator():
 
     # Generating slice refocusing gradient
     slice_selection_half_area = (((ramp + plateau))/2)*grad_slisel_amplitude
-    grad_sliref_waveform, grad_sliref_amplitude, ru_us, rd_us, pt_us = min_trap_grad(slice_selection_half_area, 22, 45, dt)
+    grad_sliref_waveform, grad_sliref_amplitude, ru_us, rd_us, pt_us = ramp_sampled_trap_grad(slice_selection_half_area, 22, 45, dt)
 
     # Generating crusher gradients
     dephasing = 10 * np.pi
     crusher_gradient_area = (dephasing*1e3) / (2*np.pi*gamma*1e-6 * 5e-3)  # in mT/m/s
-    grad_crush_waveform, grad_crush_amplitude, ru_us, rd_us, pt_us = min_trap_grad(crusher_gradient_area, 22, 45, dt)
+    grad_crush_waveform, grad_crush_amplitude, ru_us, rd_us, pt_us = ramp_sampled_trap_grad(crusher_gradient_area, 22, 45, dt)
     for i, value in enumerate(grad_crush_waveform[0]):
         grad_crush_waveform[0][i] = np.round(value, 4)  # rounding to 4 decimal places for mT/m
 
@@ -178,8 +180,12 @@ def se2d_generator():
     
     # Event
     equation_ref_slice_ref_gradient_event = EquationRef(type = "equation",
-                                                         equation = "equation_slice_ref_gradient_event")
-    equation_slice_ref_gradient_event = Equation(equation = "set(TE)/2 - " + str(grad_slice_select_refocus.duration/2))
+                        equation = "equation_slice_ref_gradient_event")
+    equation_slice_ref_gradient_event = Equation(equation = "set(TE)/2" + \
+                        " - " + str(grad_slice_select_refocus.duration/2) + \
+                        " + " + str(rfpulse.duration/2) + \
+                        " + " + str(rf_event.time))
+        
     slice_ref_gradient_event = Grad(
         axis="slice",
         object="grad_slice_select_refocus",
@@ -202,9 +208,13 @@ def se2d_generator():
     
     # Events
     equation_ref_crusher_gradient_event_s1 = EquationRef(type = "equation",
-                                                     equation = "equation_crusher_gradient_event_s1")
-    equation_crusher_gradient_event_s1 = Equation(equation = "set(TE)/2 - " + str(grad_slice_select_refocus.duration/2 + grad_crusher.duration) + " - 20")
-    start_time_crusher_gradient_event_s1 = TE/2 - (grad_slice_select_refocus.duration/2 + grad_crusher.duration) - 20
+                        equation = "equation_crusher_gradient_event_s1")
+    equation_crusher_gradient_event_s1 = Equation(equation = "set(TE)/2" + \
+                        " - " + str(grad_slice_select_refocus.duration/2 + grad_crusher.duration) + \
+                        " + " + str(rfpulse.duration/2) + \
+                        " + " + str(rf_event.time))
+    
+    #start_time_crusher_gradient_event_s1 = TE/2 - (grad_slice_select_refocus.duration/2 + grad_crusher.duration) - 20
     crusher_gradient_event_s1 = Grad(
         axis="slice",
         object="grad_crusher",
@@ -237,8 +247,11 @@ def se2d_generator():
 
     # Event 
     equation_ref_rf_ref_event= EquationRef(type = "equation",
-                                           equation = "equation_rf_ref_event")
-    equation_rf_ref_event = Equation(equation = "set(TE)/2 - " + str(rfpulse_refocusing.duration/2) + " + 20")   
+                        equation = "equation_rf_ref_event") 
+    equation_rf_ref_event = Equation(equation = "set(TE)/2" + \
+                        " - " + str(rfpulse_refocusing.duration/2) + \
+                        " + " + str(rfpulse.duration/2) + \
+                        " + " + str(rf_event.time))
     rf_ref_event = Rf(
         object="rf_refocusing",
         time=equation_ref_rf_ref_event,
@@ -249,8 +262,11 @@ def se2d_generator():
     # Defining the crusher gradients
     # Events
     equation_ref_crusher_gradient_event_s2 = EquationRef(type = "equation",
-                                                         equation = "equation_crusher_gradient_event_s2")
-    equation_crusher_gradient_event_s2 = Equation(equation = "set(TE)/2 + " + str(grad_slice_select_refocus.duration/2) + " + 20")
+                       equation = "equation_crusher_gradient_event_s2")
+    equation_crusher_gradient_event_s2 = Equation(equation = "set(TE)/2" + \
+                       " + " + str(grad_slice_select_refocus.duration/2) + \
+                       " + " + str(rfpulse.duration/2) + \
+                       " + " + str(rf_event.time))
     crusher_gradient_event_s2 = Grad(
         axis="slice",
         object="grad_crusher",
@@ -266,9 +282,11 @@ def se2d_generator():
 
     # Defining the echo time (TE) event
     equation_ref_te_event = EquationRef(type = "equation",
-                                                         equation = "equation_te_event")
-    equation_te_event = Equation(equation = "set(TE) + " + str(grad_slice_select.duration/2))
-    te_event = Mark(time=equation_ref_te_event )
+                        equation = "equation_te_event")
+    equation_te_event = Equation(equation = "set(TE) " \
+                        " + " + str(rfpulse.duration/2) + \
+                        " + " + str(rf_event.time))
+    te_event = Mark(time=equation_ref_te_event)
     
     
     # Defining the spoiler gradient
@@ -288,8 +306,13 @@ def se2d_generator():
 
     
     # Defining the repetition time (TR) event
-    print("test " + str(TR - start_time_crusher_gradient_event_s1 - grad_crusher.duration))
-    tr_event = Mark(time=TR - start_time_crusher_gradient_event_s1 - grad_crusher.duration)
+    #print("test " + str(TR - start_time_crusher_gradient_event_s1 - grad_crusher.duration))
+    equation_ref_tr_event = EquationRef(type = "equation",
+                        equation = "equation_tr_event")
+    equation_tr_event = Equation(equation = "set(TR) - set(TE) " \
+                        " - " + str(rfpulse.duration/2) + \
+                        " - " + str(rf_event.time))
+    tr_event = Mark(time=equation_ref_tr_event ) # - start_time_crusher_gradient_event_s1 - grad_crusher.duration)
     
 
     ## Adding objects to the sequence data
@@ -408,6 +431,7 @@ def se2d_generator():
         "equation_crusher_gradient_event_s1": equation_crusher_gradient_event_s1,
         "equation_crusher_gradient_event_s2": equation_crusher_gradient_event_s2,
         "equation_te_event": equation_te_event,
+        "equation_tr_event": equation_tr_event,
         "equation_rf_ref_event": equation_rf_ref_event,
         "equation_slice_ref_gradient_event": equation_slice_ref_gradient_event
     })
@@ -416,6 +440,7 @@ def se2d_generator():
     add_cartesian_readout(sequence_data, 
                           "block_TR", 
                           "block_SE", 
+                          "block_spoiler", 
                           sequence_data.infos.fov * 1e-2,
                           sequence_data.infos.pelines)
 

simpleWaveformGenerator.py
@@ -104,9 +104,9 @@ def min_trap_grad(area, gmax, slew, dt):
     print("plateau points", (len(normalized_trap) - 2 * ramppts))
     print("amplitude", amplitude)
 
-    ramp_up_time = ramppts * dt * 1e6  # convert to us
-    ramp_down_time = ramppts * dt * 1e6  # convert to us
-    plateau_time = (len(normalized_trap) - 2 * ramppts) * dt * 1e6  # convert to us
+    ramp_up_time = ramppts * dt * 1e6  # convert to 1e-6 us
+    ramp_down_time = ramppts * dt * 1e6  # convert to 1e-6 us
+    plateau_time = (len(normalized_trap) - 2 * ramppts) * dt * 1e6  # convert to 1e-6 us
 
     return np.expand_dims(normalized_trap, axis=0), amplitude, int(ramp_up_time), int(ramp_down_time), int(plateau_time)
 
@@ -132,7 +132,7 @@ def ramp_sampled_trap_grad(area, gmax, slew, dt, *args):
 
     """
 
-    slew = slew * 1e3  # convert slew rate to mT/m/s
+    slew = slew * 1e-3  # convert slew rate to mT/m/s
 
     if len(args) < 5:
         # in case we are making a rewinder